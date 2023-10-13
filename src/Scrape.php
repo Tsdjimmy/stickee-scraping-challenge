26254196da01895ef63c161dd863384803d4bb11
tsdjimmy <oluwasegunjimoh@gmail.com>
2023-10-13 11:55:41 +0100

    Fix: Update to address date and color variants

--- src/Scrape.php
+++ src/Scrape.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace App;
+use App\ScrapeHelper;
 
 require 'vendor/autoload.php';
 
 class Scrape
 {
+    const BASE_URL = 'https://www.magpiehq.com/developer-challenge/smartphones';
+    const TITLE_SELECTOR = '.product-name';
+    const CAPACITY_SELECTOR = '.product-capacity';
+    const PRICE_SELECTOR = '.text-lg';
+    const AVAILABILITY_SELECTOR = '.text-sm.block.text-center';
+    const SHIPPING_SELECTOR = '.text-sm.block.text-center';
+    const IMAGE_SELECTOR = 'img';
+    const COLOUR_SELECTOR = '[data-colour]';
+
     public function run(): array
     {
-        $url = 'https://www.magpiehq.com/developer-challenge/smartphones';
+        $url = self::BASE_URL;
         $products = [];
 
         $page = 1;
-        $uniqueTitles = []; // This is to keep track of unique product titles
 
         while (true) {
             $pageUrl = $url . '?page=' . $page;
@@ -25,42 +33,57 @@ class Scrape
 
             $crawler = $html;
 
-            $crawler->filter('.product')->each(function ($node) use (&$products, $url, &$uniqueTitles) {
-                $title = $node->filter('.product-name')->text();
-
-                // Checks if the title is already in the $uniqueTitles
-                if (in_array($title, $uniqueTitles)) {
-                    return;
-                }
-
-                $uniqueTitles[] = $title;
-
-                $capacity = $node->filter('.product-capacity')->text();
-                $priceText = $node->filter('.text-lg')->text();
+            $crawler->filter('.product')->each(function ($node) use (&$products, $url) {
+                $title = $node->filter(self::TITLE_SELECTOR)->text();
+                $capacity = $node->filter(self::CAPACITY_SELECTOR)->text();
+                $priceText = $node->filter(self::PRICE_SELECTOR)->text();
                 $price = (float) str_replace(['£', '£'], '', $priceText);
-                $availabilityText = $node->filter('.text-sm.block.text-center')->first()->text();
+                $availabilityText = $node->filter(self::AVAILABILITY_SELECTOR)->first()->text();
                 $availabilityText = trim(str_replace('Availability:', '', $availabilityText));
-                $shippingText = $node->filter('.text-sm.block.text-center')->last()->text();
+                $shippingText = $node->filter(self::SHIPPING_SELECTOR)->last()->text();
                 $shippingText = trim(str_replace('Availability:', '', $shippingText));
-                $colour = $node->filter('[data-colour]')->attr('data-colour');
-                $imageUrl = $node->filter('img')->attr('src');
+                $imageUrl = $node->filter(self::IMAGE_SELECTOR)->attr('src');
                 $imageUrl = str_replace('..', '', $imageUrl);
                 $imageUrl = $url . '/' . ltrim($imageUrl, '/');
                 $isAvailable = stripos($availabilityText, 'in stock') !== false;
                 $shippingDate = date('Y-m-d', strtotime(str_replace('Delivery from ', '', $shippingText)));
 
-                $product = [
-                    'title' => $title,
-                    'price' => $price,
-                    'imageUrl' => $imageUrl,
-                    'capacityMB' => $this->convertCapacityToMB($capacity),
-                    'colour' => $colour,
-                    'availabilityText' => $availabilityText,
-                    'isAvailable' => $isAvailable,
-                    'shippingText' => $shippingText,
-                    'shippingDate' => $shippingDate,
-                ];
-                $products[] = $product;
+                // Fetch color variants
+                $colorVariants = $node->filter(self::COLOUR_SELECTOR);
+                $colors = $colorVariants->each(function ($colorNode) use ($title, $capacity, $price, $availabilityText, $shippingText, $imageUrl, $isAvailable, $shippingDate) {
+                    $colour = $colorNode->attr('data-colour');
+                    $product = [
+                        'title' => $title,
+                        'price' => $price,
+                        'imageUrl' => $imageUrl,
+                        'capacityMB' => $this->convertCapacityToMB($capacity),
+                        'colour' => $colour,
+                        'availabilityText' => $availabilityText,
+                        'isAvailable' => $isAvailable,
+                        'shippingText' => $shippingText,
+                        'shippingDate' => $shippingDate,
+                    ];
+                    return $product;
+                });
+
+                // Deduplicates products based on title, color, and capacity
+                foreach ($colors as $colorProduct) {
+                    $isDuplicate = false;
+                    foreach ($products as $existingProduct) {
+                        if (
+                            $existingProduct['title'] === $colorProduct['title']
+                            && $existingProduct['colour'] === $colorProduct['colour']
+                            && $existingProduct['capacityMB'] === $colorProduct['capacityMB']
+                        ) {
+                            $isDuplicate = true;
+                            break;
+                        }
+                    }
+
+                    if (!$isDuplicate) {
+                        $products[] = $colorProduct;
+                    }
+                }
             });
 
             $nextPageLink = $crawler->filter('#pages .flex-wrap a.active');
@@ -70,6 +93,25 @@ class Scrape
 
             $page++;
         }
+
+        // Extracts the date from shippingText and update shippingDate
+        foreach ($products as &$product) {
+            $shippingText = $product['shippingText'];
+            if (preg_match('/\d{4}-\d{2}-\d{2}/', $shippingText, $matches)) {
+                $shippingDate = $matches[0];
+            } else {
+                if (preg_match('/(\d{1,2}(?:st|nd|rd|th)? [A-Za-z]+ \d{4})/', $shippingText, $matches)) {
+                    $dateString = $matches[0];
+                    $dateString = preg_replace('/(st|nd|rd|th)/', '', $dateString);
+                    $shippingDate = date('Y-m-d', strtotime($dateString));
+                } else {
+                    $shippingDate = "";
+                }
+            }
+            $product['shippingDate'] = $shippingDate;
+        }
+
+
 
         return $products;
     }
